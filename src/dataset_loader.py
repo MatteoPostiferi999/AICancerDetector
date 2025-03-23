37d4993e21c4529ec9e56f0cac7a3eb1c508a237
MatteoPostiferi999 <matteo.postiferi@gmail.com>
2025-03-23 21:05:57 +0100

    dataset_loader.py points to the correct path on Google Colab

--- src/dataset_loader.py
+++ src/dataset_loader.py
@@ -13,11 +13,18 @@ transform = transforms.Compose([
 
 
 # Funzione per caricare dataset + dataloader
-def get_dataloader(
-        data_dir="datasets/dataset_prepared/train",  # path di default
+
+def get_dataloader(data_dir=None, batch_size=32, shuffle=True):
+    if data_dir is None:
+        # Usa il path corretto a seconda dell'ambiente
+        if "COLAB_GPU" in os.environ:
+            data_dir = "/content/dataset"
+        else:
+            data_dir = "datasets/dataset_prepared/train"
+
         batch_size=32,
         shuffle=True
-):
+
     dataset = ImageFolder(root=data_dir, transform=transform)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
 
